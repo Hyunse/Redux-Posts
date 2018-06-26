9a651f76650220f7d74c4433011cf37947d4e770
KimHyunse <anzmf12@naver.com>
2018-06-26 13:47:22 -0400

    Show Post List / Link Post New

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
-
+import { Link } from 'react-router-dom';
+import _ from 'lodash';
 
 class PostIndex extends Component {
   
@@ -9,11 +10,31 @@ class PostIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
     console.log(this.props.posts);
     return (
       <div>
-        PostIndex
+        <div className="text-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        <h2>PostIndex!</h2>
+        <div>
+          <ul className="list-group">
+            {this.renderPosts()}
+          </ul>
+        </div>
       </div>
     );
   }
---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Posts New!</h2>
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
